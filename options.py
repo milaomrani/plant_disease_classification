60b10476742ea23949664afe70f16bb022700b19
Milad Omrani <50385765+milaomrani@users.noreply.github.com>
2023-06-30 12:58:05 -0400

    Update options.py

--- options.py
+++ options.py
@@ -9,5 +9,8 @@ def parse_arguments():
     parser.add_argument("--hidden_units", type=int, default=10, help="Number of hidden units in the model")
     parser.add_argument("--lr", type=float, default=0.001, help="Learning rate for the optimizer")
     parser.add_argument("--device", type=str, default="cuda", help="Device to use for training (cuda or cpu)")
+    parser.add_argument("--model_path", type=str, help="Path to save the trained model")
+    parser.add_argument("--load_size", type=int, default=64, help="image size")
+    parser.add_argument("--model", type=str, default="TinyVgg", help="Model architecture: TinyVgg or ImageNet")
     args = parser.parse_args()
     return args
